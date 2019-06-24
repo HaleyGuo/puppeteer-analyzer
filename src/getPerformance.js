a4cd250b9706c33085ff1fd031b23c47110dbc00
guohm <guohm@rd.netease.com>
2019-06-24 23:13:29 +0800

    add page.on and listLinksSpa

--- src/getPerformance.js
+++ src/getPerformance.js
@@ -4,26 +4,43 @@ const {
  } = require('./parseData');
 
 async function getPerformance(page) {
-    await page.goto('https://note.youdao.com/web/#/file/recent/note/0538AC4396B3481AA3C276AF73425736/');
-    await page.waitFor(1000);
-    const performanceMetrics = await page._client.send('Performance.getMetrics');
+  let listLinksSpa;
+  page.on('metrics', ({ title, metrics }) => {
+    if (title === 'listLinksSpa') {
+      listLinksSpa = metrics.Timestamp * 1000;
+      // debugger;
+    }
+  });
 
+  await page.goto('https://note.youdao.com/web/#/file/recent/note/0538AC4396B3481AA3C276AF73425736/');
+  const performanceMetrics = await page._client.send('Performance.getMetrics');
+
+  const navigationStart = getTimeFromPerformanceMetrics(
+    performanceMetrics,
+    'NavigationStart'
+  );
+  await page.waitFor(1000);
+
+  return {
+    listLinksSpa: listLinksSpa - navigationStart,
+  };
+  // 第一版
+  // const performanceTiming = JSON.parse(
+  //   await page.evaluate(() => JSON.stringify(window.performance.timing))
+  // );
+  // return extractDataFromPerformanceTiming(
+  //   performanceTiming,
+  //   'responseEnd',
+  //   'domInteractive',
+  //   'domContentLoadedEventEnd',
+  //   'loadEventEnd'
+  // );
+
+  // 第二版
+  // return extractDataFromPerformanceMetrics(
+  //   performanceMetrics,
+  //   'FirstMeaningfulPaint'
+  // );
+}
   
-    const performanceTiming = JSON.parse(
-      await page.evaluate(() => JSON.stringify(window.performance.timing))
-    );
-  
-    // return extractDataFromPerformanceTiming(
-    //   performanceTiming,
-    //   'responseEnd',
-    //   'domInteractive',
-    //   'domContentLoadedEventEnd',
-    //   'loadEventEnd'
-    // );
-    return extractDataFromPerformanceMetrics(
-      performanceMetrics,
-      'FirstMeaningfulPaint'
-    );
-  }
-  
-  module.exports = getPerformance;
+module.exports = getPerformance;
