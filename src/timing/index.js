d52ef41452131cd89916e4dbd54ed6fc7d9e86d2
guohm <guohm@rd.netease.com>
2019-07-04 21:51:52 +0800

    add       let {

--- src/timing/index.js
+++ src/timing/index.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
     await page.goto('https://note.youdao.com/web/#/file/recent/note/0538AC4396B3481AA3C276AF73425736/');
 
     const performanceTiming = JSON.parse(
-        await page.evaluate(() => JSON.stringify(window.performance.timing))
+        await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType("paint")))
     );
     console.log(performanceTiming);
 
